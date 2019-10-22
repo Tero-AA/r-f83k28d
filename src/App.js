0fbaeac0d40fcbbfc4a6227c38223efadacbbc72
Andrés Ayala <53452577+Tero-AA@users.noreply.github.com>
2019-10-22 11:00:17 -0500

    add counter functionality

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      counter: 0
+    }
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.handleClick} >Incrementa</button>
       </div>
     );
   }
+
+  handleClick() {
+    this.setState({ counter: this.state.counter + 1 })
+  }
 }
+
+
 
 export default App;
